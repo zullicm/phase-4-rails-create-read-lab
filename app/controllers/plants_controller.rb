0137e600116b171a3133dae412a166e0d7a6008d
Christopher Marzulli <chriszulli15@gmail.com>
2022-10-04 16:22:33 -0400

    commit tree 04a28bcc886facff2277b375854b6f8b8716777f

---
+++ app/controllers/plants_controller.rb
@@ -0,0 +1,28 @@
+class PlantsController < ApplicationController
+
+  def index 
+    plants = Plant.all
+    render json: plants
+  end
+
+  def show
+    plant = Plant.find_by(id: params[:id])
+    if plant
+      render json: plant
+    else
+      render json: { error: "Plant not found" }, status: :not_found
+    end
+  end
+
+  def create
+    plant = Plant.create(plant_params)
+    render json: plant, status: :created
+  end
+
+  private
+
+  def plant_params
+    params.permit(:name, :image, :price)
+  end
+
+end
